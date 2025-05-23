9da423ab3c9586e0ccecaea6b46f516f9bcf071a
ammartalpur <ammartalpur69@gmail.com>
2025-05-23 10:58:58 +0500

    Initial commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import AuthFrom from "@/components/AuthForm";
+import React from "react";
+
+const SignIn = () => {
+  return <AuthFrom type="sign-in" />;
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import AuthFrom from "@/components/AuthForm";
+import React from "react";
+
+const SignUp = () => {
+  return <AuthFrom type="sign-up" />;
+};
+
+export default SignUp;
